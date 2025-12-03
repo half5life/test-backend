71760fa16d97ee89d6f592603bdbe049bd6bb3a0
half5life <66329306+half5life@users.noreply.github.com>
2025-12-04 01:53:13 +0300

    add logout endpoint with token blacklist to meet requirements

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import RegisterView, UserProfileView, RoleViewSet
+from .views import RegisterView, UserProfileView, RoleViewSet, LogoutView
 
 router = DefaultRouter()
 router.register(r'roles', RoleViewSet)
@@ -13,6 +13,7 @@ urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='auth_register'),
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/logout/', LogoutView.as_view(), name='auth_logout'),
     path('auth/me/', UserProfileView.as_view(), name='user_profile'),
     path('', include(router.urls)),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
+from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserRegistrationSerializer, UserProfileSerializer, RoleSerializer
 from django.contrib.auth import get_user_model
 from .models import Role
@@ -38,4 +41,17 @@ class RoleViewSet(viewsets.ModelViewSet):
     serializer_class = RoleSerializer
     permission_classes = (IsAuthenticated, RbacPermission)
     resource_name = 'roles'
+
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
